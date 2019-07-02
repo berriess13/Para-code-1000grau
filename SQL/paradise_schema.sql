64afc22d50b3e2be2660cae14394c6ed5a9759e6
U.S.Swagger Captain <52402740+berriess13@users.noreply.github.com>
2019-07-01 22:19:56 -0300

    Fixed SQL Errors
    Removed useless stuff

--- SQL/paradise_schema.sql
+++ SQL/paradise_schema.sql
@@ -264,10 +264,6 @@ CREATE TABLE `player` (
   `windowflashing` smallint(4) DEFAULT '1',
   `ghost_anonsay` tinyint(1) NOT NULL DEFAULT '0',
   `exp` mediumtext,
-  `clientfps` smallint(4) DEFAULT '0',
-  `atklog` smallint(4) DEFAULT '0',
-  `fuid` BIGINT(20) NULL DEFAULT NULL,
-  `fupdate` SMALLINT(4) NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ckey` (`ckey`)
 ) ENGINE=InnoDB AUTO_INCREMENT=32446 DEFAULT CHARSET=latin1;
@@ -362,12 +358,12 @@ DROP TABLE IF EXISTS `privacy`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `privacy` (
-  `ckey` varchar(32) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL,
-  `consent` bit(1) NOT NULL,
-  PRIMARY KEY (`ckey`),
-  UNIQUE KEY `ckey_UNIQUE` (`ckey`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ckey` varchar(32) NOT NULL,
+  `option` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -523,29 +519,3 @@ CREATE TABLE `memo` (
   PRIMARY KEY (`ckey`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `ipintel`
---
-DROP TABLE IF EXISTS `ipintel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE  `ipintel` (
-`ip` INT UNSIGNED NOT NULL ,
-`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL ,
-`intel` REAL NOT NULL DEFAULT  '0',
-PRIMARY KEY (  `ip` )
-) ENGINE = INNODB;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vpn_whitelist`
---
-DROP TABLE IF EXISTS `vpn_whitelist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `vpn_whitelist` (
-  `ckey` VARCHAR(32) NOT NULL,
-  `reason` text
-  PRIMARY KEY (`ckey`)
-) ENGINE=INNODB;
